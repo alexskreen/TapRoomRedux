6a8a332e2c9c962f74a45b20cdd930b99afad421
Alex Skreen <skreenalex@gmail.com>
2020-05-01 16:02:20 -0700

    refactor KegControl

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -11,8 +11,7 @@ class KegControl extends React.Component {
     super(props);
     console.log(props);
     this.state = {
-      formVisibleOnPage: false,
-      selectedKeg: null,
+     selectedKeg: null,
       editing: false,
     };
   }
@@ -20,14 +19,15 @@ class KegControl extends React.Component {
   handleClick = () => {
     if (this.state.selectedKeg != null) {
       this.setState({
-        formVisibleOnPage: false,
         selectedKeg: null,
         editing: false,
       });
     } else {
-      this.setState((prevState) => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage,
-      }));
+      const { dispatch } = this.props;
+      const action = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action);
     }
   };
 
@@ -44,7 +44,10 @@ class KegControl extends React.Component {
       pints: pints,
     };
     dispatch(action);
-    this.setState({ formVisibleOnPage: false });
+    const action2 = {
+      type: 'TOGGLE_FORM'
+    }
+    dispatch(action2);
   };
 
   handleChangingSelectedKeg = (id) => {
@@ -105,7 +108,7 @@ class KegControl extends React.Component {
         />
       );
       buttonText = "Return to Keg List";
-    } else if (this.state.formVisibleOnPage) {
+    } else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = (
         <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />
       );
