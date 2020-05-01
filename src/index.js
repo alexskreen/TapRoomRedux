aee338ee053df4a7071fcff3262c8efb1686f7f5
Alex Skreen <skreenalex@gmail.com>
2020-05-01 14:22:21 -0700

    fix typo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers/keg-list-reducer';
-import { Provider } from 'reaact-redux';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
