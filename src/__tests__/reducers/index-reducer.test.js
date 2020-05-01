d02a4095ffda28320cc8c220a104f9f23395eaba
Alex Skreen <skreenalex@gmail.com>
2020-05-01 15:29:36 -0700

    pass second test

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from "../../reducers/index";
+import { createStore } from "redux";
+import formVisibleReducer from "../../reducers/form-visible-reducer";
+import kegListReducer from "../../reducers/keg-list-reducer";
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   test("Should return default state if no action type is recognized", () => {
@@ -7,4 +12,11 @@ describe("rootReducer", () => {
       formVisibleOnPage: false,
     });
   });
+
+  test("Check that initial state of formVisibleReducer matches root reducer", () => {
+    expect(store.getState().formVisibleOnPage).toEqual(
+      formVisibleReducer(undefined, { type: null })
+    );
+  });
+
 });
