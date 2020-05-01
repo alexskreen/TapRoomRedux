294c0bbd70a562caa04f787bb1d01e5733cff980
Alex Skreen <skreenalex@gmail.com>
2020-05-01 10:13:19 -0700

    pass first test, fail second

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,45 @@
 import kegListReducer from "../../reducers/keg-list-reducer";
 
 describe("kegListReducer", () => {
+
+  let action;
+  const kegData = {
+    brewery: "Great Notion",
+    beer: "Over Ripe",
+    beerType: "IPA - NE",
+    ABV: "7%",
+    pints: 124,
+    id: 1
+  }
+
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test("Should successfully add new keg data to masterKegList", () => {
+    const { brewery, beer, beerType, ABV, pints, id } = kegData;
+    action = {
+      type: "ADD_KEG",
+      brewery: brewery,
+      beer: beer,
+      beerType: beerType,
+      ABV: ABV,
+      pints: pints,
+      id: id,
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        brewery: brewery,
+        beer: beer,
+        beerType: beerType,
+        ABV: ABV,
+        pints: pints,
+        id: id,
+      },
+    });
+  });
+
+
+
 });
