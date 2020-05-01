f60895c2df7c6a0b3174af2241ee4ddeb2251714
Alex Skreen <skreenalex@gmail.com>
2020-05-01 15:16:00 -0700

    fail first form-visible reducer test

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -4,4 +4,6 @@ describe("formVisibleReducer", () => {
   test("Should return default state if no action type is recognized", () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false);
   });
+
+
 });
