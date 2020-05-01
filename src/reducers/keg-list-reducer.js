93f0bb86041eb97a92361856999aba2f1faac499
Alex Skreen <skreenalex@gmail.com>
2020-05-01 10:18:34 -0700

    pass second test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    case "ADD_KEG":
+      const { brewery, beer, beerType, ABV, pints, id } = action;
+      return Object.assign({}, state, {
+        [id]: {
+          brewery: brewery,
+          beer: beer,
+          beerType: beerType,
+          ABV: ABV,
+          pints: pints,
+          id: id,
+        },
+      });
+    default:
+      return state;
+  }
 };
